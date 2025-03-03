eb389fbdf9d5d03c8d5d5a92f0bebdb1fde78d3d
tiavina-mika <tiavinamika@gmail.com>
2025-03-03 15:34:47 +0300

    Create data.ts

---
+++ src/utils/data.ts
@@ -0,0 +1,286 @@
+export const packagingExecutions = [
+  {
+    uniqueCode: 'RCP1',
+    recipeName: "Patate douce",
+    recipe: {
+      objectId: 'recipe1',
+      uniqueCode: 'RCP1',
+      name: "Patate douce",
+      sections: [
+        {
+          objectId: 'section11',
+          name: 'Section 11',
+        },
+        {
+          objectId: 'section12',
+          name: 'Section 12',
+        }
+      ]
+    },
+    sections: [
+      {
+        section: {
+          objectId: 'section11',
+          name: 'Section 11',
+        },
+        sectionName: 'Section 11',
+      },
+      {
+        section: {
+          objectId: 'section12',
+          name: 'Section 12',
+        },
+        sectionName: 'Section 12',
+        counterWeighing: {
+          weight: 13,
+          reason: 'broken'
+        }
+      }
+    ]
+  },
+  {
+    uniqueCode: 'RCP2',
+    recipeName: "Pizza Margherita",
+    recipe: {
+      objectId: 'recipe2',
+      uniqueCode: 'RCP2',
+      name: "Pizza Margherita",
+      sections: [
+        {
+          objectId: 'section21',
+          name: 'Section 21',
+        },
+        {
+          objectId: 'section22',
+          name: 'Section 22',
+        }
+      ]
+    },
+    sections: [
+      {
+        section: {
+          objectId: 'section21',
+          name: 'Section 21',
+        },
+        sectionName: 'Section 21',
+      },
+      {
+        section: {
+          objectId: 'section22',
+          name: 'Section 22',
+        },
+        sectionName: 'Section 22',
+      }
+    ]
+  },
+  {
+    uniqueCode: 'RCP3',
+    recipeName: "Poulet rôti",
+    recipe: {
+      objectId: 'recipe3',
+      uniqueCode: 'RCP3',
+      name: "Poulet rôti",
+      sections: [
+        {
+          objectId: 'section31',
+          name: 'Section 31',
+        },
+        {
+          objectId: 'section32',
+          name: 'Section 32',
+        }
+      ]
+    },
+    sections: [
+      {
+        section: {
+          objectId: 'section31',
+          name: 'Section 31',
+        },
+        sectionName: 'Section 31',
+      },
+      {
+        section: {
+          objectId: 'section32',
+          name: 'Section 32',
+        },
+        sectionName: 'Section 32',
+      }
+    ]
+  },
+  {
+    uniqueCode: 'RCP4',
+    recipeName: "Pâtes carbonara",
+    recipe: {
+      objectId: 'recipe4',
+      uniqueCode: 'RCP4',
+      name: "Pâtes carbonara",
+      sections: [
+        {
+          objectId: 'section41',
+          name: 'Section 41',
+        },
+        {
+          objectId: 'section42',
+          name: 'Section 42',
+        }
+      ]
+    },
+    sections: [
+      {
+        section: {
+          objectId: 'section41',
+          name: 'Section 41',
+        },
+        sectionName: 'Section 41',
+      },
+      {
+        section: {
+          objectId: 'section42',
+          name: 'Section 42',
+        },
+        sectionName: 'Section 42',
+      }
+    ]
+  },
+  {
+    uniqueCode: 'RCP5',
+    recipeName: "Salade César",
+    recipe: {
+      objectId: 'recipe5',
+      uniqueCode: 'RCP5',
+      name: "Salade César",
+      sections: [
+        {
+          objectId: 'section51',
+          name: 'Section 51',
+        },
+        {
+          objectId: 'section52',
+          name: 'Section 52',
+        }
+      ]
+    },
+    sections: [
+      {
+        section: {
+          objectId: 'section51',
+          name: 'Section 51',
+        },
+        sectionName: 'Section 51',
+      },
+      {
+        section: {
+          objectId: 'section52',
+          name: 'Section 52',
+        },
+        sectionName: 'Section 52',
+      }
+    ]
+  },
+  {
+    uniqueCode: 'RCP6',
+    recipeName: "Ratatouille",
+    recipe: {
+      objectId: 'recipe6',
+      uniqueCode: 'RCP6',
+      name: "Ratatouille",
+      sections: [
+        {
+          objectId: 'section61',
+          name: 'Section 61',
+        },
+        {
+          objectId: 'section62',
+          name: 'Section 62',
+        }
+      ]
+    },
+    sections: [
+      {
+        section: {
+          objectId: 'section61',
+          name: 'Section 61',
+        },
+        sectionName: 'Section 61',
+      },
+      {
+        section: {
+          objectId: 'section62',
+          name: 'Section 62',
+        },
+        sectionName: 'Section 62',
+      }
+    ]
+  },
+  {
+    uniqueCode: 'RCP7',
+    recipeName: "Poulet curry",
+    recipe: {
+      objectId: 'recipe7',
+      uniqueCode: 'RCP7',
+      name: "Poulet curry",
+      sections: [
+        {
+          objectId: 'section71',
+          name: 'Section 71',
+        },
+        {
+          objectId: 'section72',
+          name: 'Section 72',
+        }
+      ]
+    },
+    sections: [
+      {
+        section: {
+          objectId: 'section71',
+          name: 'Section 71',
+        },
+        sectionName: 'Section 71',
+      },
+      {
+        section: {
+          objectId: 'section72',
+          name: 'Section 72',
+        },
+        sectionName: 'Section 72',
+      }
+    ]
+  },
+  {
+    uniqueCode: 'RCP8',
+    recipeName: "Tarte aux pommes",
+    recipe: {
+      objectId: 'recipe8',
+      uniqueCode: 'RCP8',
+      name: "Tarte aux pommes",
+      sections: [
+        {
+          objectId: 'section81',
+          name: 'Section 81',
+        },
+        {
+          objectId: 'section82',
+          name: 'Section 82',
+        }
+      ]
+    },
+    sections: [
+      {
+        section: {
+          objectId: 'section81',
+          name: 'Section 81',
+        },
+        sectionName: 'Section 81',
+      },
+      {
+        section: {
+          objectId: 'section82',
+          name: 'Section 82',
+        },
+        sectionName: 'Section 82',
+      }
+    ]
+  },
+]
