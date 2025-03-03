5f002c93eb0bf8aaad8f7c948efc852259471952
tiavina-mika <tiavinamika@gmail.com>
2025-03-03 16:56:23 +0300

    Update utils.ts

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -8,3 +8,30 @@ export const searchRecipesByUniqueCodeOrName = (packagingExecutions: Record<stri
     return pe.uniqueCode.toLowerCase().includes(search.toLowerCase()) || pe.recipeName.toLowerCase().includes(search.toLowerCase())
   })
 }
+
+export const packagingNature = [
+	{ value: "REUSABLE", label: "Réemployable" },
+	{ value: "DISPOSABLE", label: "Jetable" },
+	{ value: "CAPPED", label: "Operculé" }
+]
+
+export const formatPackagingExecutionWeightsInitialValues = (
+  packagingExecution: Record<string, any>,
+  proposedWeightsBySections: Record<string, any> = {}
+) => {
+  return {
+    ...packagingExecution,
+    packagings: packagingExecution.packagings.map((packaging: Record<string, any>) => ({
+      ...packaging,
+      forecastNumber: packaging.forecastNumber || 0
+    })),
+    sections: packagingExecution.sections.map((section: Record<string, any>) => ({
+      ...section,
+      realWeight: 0,
+      proposedWeight: proposedWeightsBySections[section.section.objectId] || 0,
+      forecastWaste: section.forecastWaste || 0,
+      packagingForecastNumber: Infinity // won't be saved in db for display only and to ease waste calculations
+    })),
+    packagingForecastNumber: 0 // won't be saved in db for display only and to ease waste calculations
+  }
+}
