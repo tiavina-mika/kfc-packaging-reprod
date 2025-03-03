5898dee32d7f1d84fd8dfcc18a2cfcc5656c07a8
tiavina-mika <tiavinamika@gmail.com>
2025-03-03 16:56:17 +0300

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,5 +2,11 @@ export const COLORS = {
   primary: '#262626',
   error: '#F44259',
   active: '#2196F3',
-  gray: '#7C7C7C'
+  gray: '#7C7C7C',
+	PRIMARY_COLOR: "#2196f3",
+	HEADER_BLUE: "#F0F9FF",
+	LABEL_GREY: "#555555",
+	PRODUCTION_STEPS_DISABLE_TEXT: "#9B9B9B",
+	BLACK800: "#262626",
+	BACKGROUND_GREY: "#F8F8F8",
 }
