3af5c54eacce63c1d6f2b622c586211d9a752f9f
tiavina-mika <tiavinamika@gmail.com>
2025-03-12 15:36:05 +0300

    Update packagingExecutionUtils.ts

--- src/utils/packagingExecutionUtils.ts
+++ src/utils/packagingExecutionUtils.ts
@@ -1,5 +1,61 @@
 import { convertGramsIntoKilos } from "./utils"
 
+export const getOrderedPackagings = (packagings: Record<string, any>[] = []) => {
+  const order = ["CAPPED", "REUSABLE", "DISPOSABLE"];
+  return packagings.sort((a, b) => order.indexOf(a.type) - order.indexOf(b.type));
+}
+
+export const formatPackagingExecutionWeightsInitialValues = (
+  packagingExecution: Record<string, any>,
+  // from data api
+  proposedWeightsBySections: Record<string, any> = {},
+  tempRealNumber = 0
+) => {
+  const orderPackagins = getOrderedPackagings(packagingExecution.packagings) || []
+  const packagings: Record<string, any>[] = []
+  let total = 0
+  let eachRealizedNumber = tempRealNumber
+
+  for (const packaging of orderPackagins) {
+    const min = Math.min(eachRealizedNumber, packaging.theoreticalNumber)
+
+    packagings.push({
+      ...packaging,
+      realizedNumber: min,
+      forecastNumber: packaging.forecastNumber || 0
+    })
+    eachRealizedNumber -= min
+    total += min
+  }
+
+  const cappedPackaging = packagings.find((packaging) => packaging.type === "CAPPED")
+  const cappedPackagingWeight = cappedPackaging ? (cappedPackaging.realizedNumber - cappedPackaging.theoreticalNumber) : 0
+
+  const sections: Record<string, any>[] = []
+
+  for (const section of packagingExecution.sections) {
+    const values = {
+      ...section,
+      realWeight: 0,
+      proposedWeight: proposedWeightsBySections[section.section.objectId] || 0,
+      forecastWaste: section.forecastWaste || 0,
+      cappedPackagingWeight: cappedPackagingWeight,
+      packagingForecastNumber: Infinity // won't be saved in db for display only and to ease waste calculations
+    }
+
+    sections.push(values)
+  }
+
+  return {
+    ...packagingExecution,
+    expectedPackagingNumber: packagingExecution.expectedPackagingNumber || 0,
+    packagings,
+    sections,
+    totalRealizedNumber: total,
+    packagingForecastNumber: 0 // won't be saved in db for display only and to ease waste calculations
+  }
+}
+
 export const calculatePackagingForecastNumberBySection = (section: Record<string, any>, realWeight = 1): number => {
   const realWeightKilos = convertGramsIntoKilos(realWeight)
   const sectionProductionWeight = section.initialProductionWeight // already in kilos
