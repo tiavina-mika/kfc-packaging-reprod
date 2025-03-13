e5fdfe24187cc65c46c7f8060f491c0d776f2a5f
tiavina-mika <tiavinamika@gmail.com>
2025-03-13 12:27:42 +0300

    Update Footer.tsx

--- src/containers/Footer.tsx
+++ src/containers/Footer.tsx
@@ -1,21 +1,36 @@
 import { Container, Typography, Link, Box } from '@mui/material';
 
+const linkOptions = [
+  {
+    label: 'Site Web',
+    url: 'https://tiavina-michael-ralainirina.onrender.com/',
+  },
+  {
+    label: 'LinkedIn',
+    url: 'https://www.linkedin.com/in/tiavina-michael-ralainirina',
+  },
+  {
+    label: 'Youtube',
+    url: 'https://www.youtube.com/@tiavina-mika',
+  },
+  {
+    label: 'Email',
+    url: 'mailto:tiavinamika@gmail.com',
+  }
+];
+
 const Footer = () => {
   return (
     <Box component="footer" sx={{ py: 3, px: 2, mt: 'auto', backgroundColor: '#000', color: 'white' }}>
       <Container maxWidth="lg">
         <Typography variant="body1">
-          <Link href="#" color="inherit" underline="none">
-            Home
-          </Link>{' '}
-          |{' '}
-          <Link href="#" color="inherit" underline="none">
-            About
-          </Link>{' '}
-          |{' '}
-          <Link href="#" color="inherit" underline="none">
-            Contact
-          </Link>
+          {linkOptions.map((link, index) => (
+            <span key={index}>
+              <Link href={link.url} color="inherit" sx={{ mr: 2 }}>
+                {link.label}
+              </Link>
+            </span>
+          ))}
         </Typography>
         <Typography variant="body2" color="inherit" align="center" sx={{ mt: 1 }}>
           {'© '}
